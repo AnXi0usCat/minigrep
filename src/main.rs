f495f7af940f670e48eebf0bf1aaecb219b3763b
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2021-12-22 15:53:31 +0000

    read contents of the file and print them out

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    let args = env::args().into_iter().skip(1).collect::<Vec<String>>();
+    let args = env::args().skip(1).collect::<Vec<String>>();
     let query = &args[0];
     let filename = &args[1];
 
     println!("Searching for: {}", query);
     println!("In the file: {}", filename);
 
-    
+    let text = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", text)
 }
