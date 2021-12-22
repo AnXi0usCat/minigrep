862e0db5dc38c94d6caa2f1275a84a227ea8ad67
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2021-12-22 15:38:28 +0000

    print comand line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().into_iter().skip(1).collect::<Vec<String>>();
+    let query = &args[0];
+    let filename = &args[1];
+
+    println!("Searching for: {}", query);
+    println!("In the file: {}", filename);
+
+    
 }
