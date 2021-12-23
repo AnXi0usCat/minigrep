815ebcad69f6b4fde7ad0fdef5fbaf2d1fa9eab8
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2021-12-23 13:29:06 +0000

    write errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use std::env;
 use std::process;
+use colored::Colorize;
 use minigrep::Config;
 
 fn main() {
     let args = env::args().skip(1).collect::<Vec<String>>();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!( + " {}", String::from(err).red());
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
