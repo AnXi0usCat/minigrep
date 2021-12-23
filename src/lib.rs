426157036ba7242268050eed6011019b5405a932
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2021-12-23 12:17:44 +0000

    add a search function and unit tests module

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,34 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let text = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", text);
-
+    for line in search(&config.query, &text) {
+        println!("{}", line)
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: & str, text: &'a str) -> Vec<&'a str> {
+    let mut result = vec![];
+    for line in text.lines() {
+        if line.contains(query) {
+            result.push(line)
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
 }
